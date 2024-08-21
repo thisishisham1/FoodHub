7296685f842177cfa5577d3a77be113ec4f0c082
sohilaemam <117683864+sohilaemam@users.noreply.github.com>
2024-08-22 00:46:14 +0200

    view model details fragment

--- app/src/main/java/iti/example/foodhub/data/remote/source/RemoteDataSource.kt
+++ app/src/main/java/iti/example/foodhub/data/remote/source/RemoteDataSource.kt
@@ -1,8 +1,11 @@
 package iti.example.foodhub.data.remote.source
 
+import iti.example.foodhub.data.remote.responseModel.Meals
 import iti.example.foodhub.data.remote.responseModel.ResponseMealsModel
 import retrofit2.Response
 
 interface RemoteDataSource {
     suspend fun getMealsByCategory(category: String): Response<ResponseMealsModel>
+
+    suspend fun getMealsById(id: Int): Meals
 }
