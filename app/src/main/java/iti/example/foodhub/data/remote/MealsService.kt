a2ee44dbc9b11aa448e4ccbfc42c09cc74df6a67
hisham mohamed <hishamohmad19@gmail.com>
2024-08-21 21:10:11 +0300

    add permission internet and get list meal by category is done

--- app/src/main/java/iti/example/foodhub/data/remote/MealsService.kt
+++ app/src/main/java/iti/example/foodhub/data/remote/MealsService.kt
@@ -1,5 +1,12 @@
 package iti.example.foodhub.data.remote
 
+import iti.example.foodhub.data.remote.responseModel.ResponseMealsModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MealsService {
 
+    @GET("filter.php")
+    suspend fun getMealsByCategory(@Query("c") category: String): Response<ResponseMealsModel>
 }
