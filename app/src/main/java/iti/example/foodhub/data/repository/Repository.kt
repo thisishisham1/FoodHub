e28ee8ec1ee4c431657d875bd16c75d8a79bfbd1
hisham mohamed <hishamohmad19@gmail.com>
2024-08-21 16:21:46 +0300

    dependency room,retrofit,glide,okhttp done and structure file for remote done but it is empty

---
+++ app/src/main/java/iti/example/foodhub/data/repository/Repository.kt
@@ -0,0 +1,6 @@
+package iti.example.foodhub.data.repository
+
+import iti.example.foodhub.data.remote.source.RemoteDataSource
+
+class Repository(private val remoteDataSource: RemoteDataSource) {
+}
