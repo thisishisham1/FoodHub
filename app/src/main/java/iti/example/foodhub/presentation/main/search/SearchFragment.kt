13547d4eb86635b5e6970b85f86e42c412ed173e
Abdallah <abdallahabohamad12@gmail.com>
2024-08-22 19:26:50 +0300

    Finished ui search

--- app/src/main/java/iti/example/foodhub/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/iti/example/foodhub/presentation/main/search/SearchFragment.kt
@@ -5,16 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import iti.example.foodhub.R
 import iti.example.foodhub.databinding.FragmentSearchBinding
+import iti.example.foodhub.presentation.model.MealUiModel
 
 class SearchFragment : Fragment() {
+
     private lateinit var binding: FragmentSearchBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSearchBinding.inflate(layoutInflater,container,false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        setupRecyclerView()
         return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        val mealList = listOf(
+            MealUiModel("Chicken Hawaiian", "https://example.com/image1.jpg", "1"),
+            MealUiModel("Red N Hot Pizza", "https://example.com/image2.jpg", "2")
+        )
+
+        val adapter = SearchAdapter(requireContext(), mealList) { meal ->
+        }
+        binding.idRecyclerViewMeals.apply {
+            this.adapter = adapter
+        }
     }
 }
