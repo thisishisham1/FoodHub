f6dfaad72d35295f19597b9b700fa9ef664e8cf1
sohilaemam <117683864+sohilaemam@users.noreply.github.com>
2024-08-24 21:07:37 +0200

    done details fragment

---
+++ app/src/main/java/iti/example/foodhub/data/database/AppDatabase.kt
@@ -0,0 +1,29 @@
+package iti.example.foodhub.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import iti.example.foodhub.data.dao.UserDao
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
