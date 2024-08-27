1f7ddfe91a93d1e1684009a5ad6246d98d3a3b04
sohilaemam <117683864+sohilaemam@users.noreply.github.com>
2024-08-27 15:30:18 +0200

    Adapter favourite fragment

--- app/src/main/java/iti/example/foodhub/presentation/main/fav/FavouriteItemsAdapter.kt
+++ app/src/main/java/iti/example/foodhub/presentation/main/fav/FavouriteItemsAdapter.kt
@@ -5,146 +5,51 @@ import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import iti.example.foodhub.R
 import iti.example.foodhub.data.local.entity.Item
 import iti.example.foodhub.data.remote.responseModel.FavoriteItem
+import iti.example.foodhub.data.remote.responseModel.Meal
+import iti.example.foodhub.presentation.main.fav.FavouriteItemsAdapter.*
+import iti.example.foodhub.presentation.main.home.ItemsAdapter
+import iti.example.foodhub.presentation.main.home.MealDiffCallback
 import iti.example.foodhub.presentation.model.MealUiModel
 
-class FavouriteItemsAdapter(private val favouriteItems:List<FavoriteItem>,
-    private val onItemClickListener: (FavoriteItem) -> Unit):
-RecyclerView.Adapter<FavouriteItemsAdapter.ViewHolder>()
-{
-    class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
-    {
-        private val itemNameTextView: TextView = itemView.findViewById(R.id.itemNameTextView)
-        private val itemImageView: ImageView = itemView.findViewById(R.id.itemImageView)
-        private val itemCardView: CardView = itemView.findViewById(R.id.item_card)
-        private val favoriteCardView: CardView = itemView.findViewById(R.id.favoriteCardView)
-
-        fun bind(item:FavoriteItem,onItemClickListener:(FavoriteItem)->Unit)
-        {
-            itemNameTextView.text=item.name
-
-
-            Glide.with(itemView.context)
-                .load(item.imageUrl)
-                .into(itemImageView)
-
-            itemCardView.setOnClickListener { onItemClickListener(item) }
-        }
-
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view=LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_card,parent,false)
-        return ViewHolder(view)
-    }
-
-
-    override fun getItemCount(): Int =favouriteItems.size
-
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(favouriteItems[position], onItemClickListener)
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-class FavoriteItemsAdapter(private val items: List<Item>,
-                           private val onItemClickListener: (Item) -> Unit
-) : RecyclerView.Adapter<FavoriteItemsAdapter.FavouriteItemViewHolder>() {
-
-    class FavouriteItemViewHolder(itemView: View, val onClick: (Item) -> Unit) : RecyclerView.ViewHolder(itemView) {
-        val itemNameTextView: TextView = itemView.findViewById(R.id.itemNameTextView)
-        val itemImageView: ImageView = itemView.findViewById(R.id.itemImageView)
-
-        fun bind(item: Item) {
-            itemNameTextView.text = item.itemName
-
-            // Assuming youhave the image URL in your Item class
-            Glide.with(itemView.context)
-                .load( "" ) // Replace with actual image URL
-                .into(itemImageView)
+class FavouriteItemsAdapter (
+    private val favouriteItems: List<MealUiModel>,
+    private val onClick: (MealUiModel) -> Unit
+) : RecyclerView.Adapter<FavouriteItemsAdapter.FavouriteItemViewHolder>() {
+
+    inner class FavouriteItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val mealName = itemView.findViewById<TextView>(R.id.itemNameTextView)
+        private val mealImage = itemView.findViewById<ImageView>(R.id.itemImageView)
+
+        fun bind(item: MealUiModel) {
+            mealName.text = item.strMeal
+            Glide.with(itemView.context).load(item.strMealThumb).into(mealImage)
 
             itemView.setOnClickListener { onClick(item) }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteItemViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_card, parent, false
-        )
-        return FavouriteItemViewHolder(itemView, onItemClickListener)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+        return FavouriteItemViewHolder(view)
     }
-
-    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: FavouriteItemViewHolder, position: Int) {
-        val item = items[position]
-        holder.bind(item)
+        holder.bind(favouriteItems[position])
     }
+
+    override fun getItemCount(): Int = favouriteItems.size
 }
 
 
@@ -171,60 +76,8 @@ class FavoriteItemsAdapter(private val items: List<Item>,
 
 
 
-*/
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-   /* private val items:List<Item>,
-    private val onClick: (MealUiModel) -> Unit,
-    private val onFavoriteClick: (MealUiModel) -> Unit
-) :
-    androidx.recyclerview.widget.ListAdapter<MealUiModel, ItemsAdapter.ViewHolder>(MealDiffCallback()) {
-
-    class ViewHolder(
-        itemView: View,
-        val onClick: (MealUiModel) -> Unit,
-        val onFavoriteClick: (MealUiModel) -> Unit
-    ) :
-        RecyclerView.ViewHolder(itemView) {
-        private val itemNameTextView: TextView = itemView.findViewById(R.id.itemNameTextView)
-        private val itemImageView: ImageView = itemView.findViewById(R.id.itemImageView)
-        private val itemCardView: CardView = itemView.findViewById(R.id.item_card)
-        private val favoriteCardView: CardView = itemView.findViewById(R.id.favoriteCardView)
-        private var currentItem: MealUiModel? = null
-
-        init {
-            itemCardView.setOnClickListener {
-                currentItem?.let { onClick(it) }
-            }
-            favoriteCardView.setOnClickListener {
-                currentItem?.let { onFavoriteClick(it) }
-            }
-
-        }
-    }
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsAdapter.ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-
-    override fun onBindViewHolder(holder: ItemsAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-}
-
-
-   */
